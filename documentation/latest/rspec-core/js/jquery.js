49562ea02d3c05648292f6d2f050e5ba34460643
Jon Rowe <hello@jonrowe.co.uk>
2015-11-16 10:21:17 +1100

    Automated commit at 2015-11-15 23:21:15 UTC by middleman-deploy 1.0.0

--- documentation/latest/rspec-core/js/jquery.js
+++
@@ -1,1 +0,0 @@
-<section><p>You should have been automatically forwarded to <a href="/documentation/3.3/rspec-core/js/jquery.js">/documentation/3.3/rspec-core/js/jquery.js</a>.</p></section>
--- documentation/latest/rspec-rails/js/jquery.js
+++
@@ -1,1 +0,0 @@
-<section><p>You should have been automatically forwarded to <a href="/documentation/3.3/rspec-rails/js/jquery.js">/documentation/3.3/rspec-rails/js/jquery.js</a>.</p></section>
