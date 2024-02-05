359f885d85a1035056ee52a4529109a332a46e06
Jon Rowe <hello@jonrowe.co.uk>
2024-02-05 19:55:44 +0000

    Remove old build

--- config.rb
+++ config.rb
@@ -74,7 +74,7 @@ config[:ignored_sitemap_matchers][:partials] = ->(source_file, _) do
   ignored
 end
 
-set :build_dir,  ENV.fetch('BUILD_OUTPUT', 'docs')
+set :build_dir,  ENV.fetch('BUILD_OUTPUT', 'build')
 set :css_dir,    'stylesheets'
 set :js_dir,     'javascripts'
 set :images_dir, 'images'
